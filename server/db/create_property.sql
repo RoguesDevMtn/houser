05f35d4ac22d3b0bb0d2bb4c5c41116bee9d8098
Alyssa Leonard <leonard.alyssa@me.com>
2018-02-08 09:42:27 -0700

    database complete

---
+++ server/db/create_property.sql
@@ -0,0 +1,2 @@
+insert into properties (property_name, description, address, city, state, zip, image_URL, loan_amount, mortgage_amount, desired_rent, user_id)
+values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
--- server/index.js
+++ server/index.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+const session = require('express-session');
 const authBaseURL = '/api/auth';
 const propsBaseURL = '/api/properties';
 const path = require('path');
 const ac = require('./controllers/auth_controller');
 const pc = require('./controllers/props_controller');
+require('dotenv').config();
+
+const checkForSession = require('./middlewares/checkForSession');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use( session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use( checkForSession );
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, "client", "build")));  
 app.get("/", (req, res) => {  
  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+
+massive(process.env.CONNECTION_STRING).then(dbInstance => {app.set('db', dbInstance)});
 
 app.post(`${authBaseURL}/login`, ac.login);
 app.post(`${authBaseURL}/register`, ac.register);
@@ -22,7 +36,7 @@ app.post(`${authBaseURL}/logout`, ac.logout);
 
 app.post(`${propsBaseURL}/properties`, pc.postProperties);
 app.get(`${propsBaseURL}/properties`, pc.getProperties);
-app.delete(`${props.baseURL}/properties`, pc.deleteProperties);
+app.delete(`${propsBaseURL}/properties`, pc.deleteProperties);
 
 const port = 3000;
 app.listen(port, () => {console.log(`Server magic is happening on port ${port}`);});
