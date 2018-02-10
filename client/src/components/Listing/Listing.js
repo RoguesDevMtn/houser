0f5f27223db5ce8643064fdbe504a8097cbe06a4
h1jaylan:]/h1 <jaylanjones421@gmail.com>
2018-02-09 23:46:27 -0600

    created listing card js file (#11)
    * created listing card js file
    
    * added style sheet for listing

---
+++ client/src/components/Listing/Listing.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import './Listing.css'
+
+class Listing extends Component {
+  render() {
+    return (
+      <div className="listingContainer">
+        <div className="imgContainer">
+          <img src={this.props.imgUrl} alt="" />
+        </div>
+        <div className="nameDescriptionContainer">
+          <div>{`${this.props.propertyName}`}</div>
+          <div>{`${this.props.propertyDescription}`}</div>
+          <div className="detailsContainer">
+            <div className='detail'>{`Loan: ${loanAmount ? this.props.loanAmount : "$"}`}</div>
+            <div className='detail'>{`Monthly Mortgage: ${monthlyMortgage
+              ? this.props.monthlyMortgage
+              : "$"}`}</div>
+            <div className='detail'> Recommended Rent: $</div>
+            <div className='detail'>{`Desired Rent: ${desiredRent
+              ? this.props.desiredRent
+              : "$"}`}</div>
+            <div className='detail'>{`Address: ${Address ? this.props.address : ""}`}</div>
+            <div className='detail'>{`City: ${city ? this.props.city : ""}`}</div>
+            <div className='detail'>{`State: ${state ? this.props.state : ""}`}</div>
+            <div className='detail'>{`Zip: ${zip ? this.props.zip : ""}`}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  const {
+    propertyName,
+    propertyDescription,
+    loanAmount,
+    monthlyMortgage,
+    desiredRent,
+    address,
+    city,
+    state,
+    zip
+  } = state;
+  return {
+    propertyName,
+    propertyDescription,
+    loanAmount,
+    monthlyMortgage,
+    desiredRent,
+    address,
+    city,
+    state,
+    zip
+  };
+};
+
+export default connect(mapStateToProps, {
+  propertyName,
+  propertyDescription,
+  loanAmount,
+  monthlyMortgage,
+  desiredRent,
+  address,
+  city,
+  state,
+  zip
+}
+)(Listing);
