aae95b7373f8c7a0e56712c99ce922db87a69783
h1jaylan:]/h1 <jaylanjones421@gmail.com>
2018-02-10 11:50:19 -0600

    Dashboard+listing styling (#14)
    * first draft of dash
    
    * created style sheet for dashboard
    
    * added more styling
    
    * adding styling to listing card

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import{ updateDesiredRent,resetFilter } from '../ducks/reducer'
+import './Dashboard.css';
+
+class Dashboard extends Component {
+    
+    render() {
+        //var listings = forEach property create a listing card?
+        return (
+            <div className='dashContainer'>
+                <div className='dashHead'>
+                    <div className="addButton">Add new property</div>
+                    <div className='desiredRentContainer'>
+                        <div>List properties with "desired rent" greator than: $</div>
+                        <input onChange={e=>updateDesiredRent(e.target.value)} className="desiredInput"type="number" name="desired rent" id=""/>
+                        <div onClick={}className='filterButton'>Filter</div>
+                        <div onClick={resetFilter()} className='resetButton'>Reset</div>
+                    </div>
+                </div>
+                <hr/>
+                <div className='homeListings'>
+                    <h2>Home Listings</h2>
+                    {/*listings*/}
+
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+  const {
+
+  } = state;
+  return {
+     updateDesiredRent, resetFilter, 
+  };
+};
+
+export default connect(mapStateToProps, {
+    updateDesiredRent, resetFilter
+}
+)(Dashboard);
