808ce2b04188a245baccad036bc1454f5cd4ada8
Michael J Cooper <ballermjc@gmail.com>
2018-02-09 18:32:45 -0700

    basic navbar added w/styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import router from './router'
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+      <Navbar />
         <div className="App">
         {/* <h1>App Is Running</h1> */}
           {/* {router} */}
+          
         </div>
       </div>
       
