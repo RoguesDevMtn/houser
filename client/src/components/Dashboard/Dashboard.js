9bac92564c5a86cdf467a07c6c3710a15adf93c0
Jaylan Jones <jaylanjones421@gmail.com>
2018-02-10 00:23:22 -0600

    first draft of dash

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import{ updateDesiredRent,resetFilter } from '../ducks/reducer'
+
+class Dashboard extends Component {
+   
+    render() {
+        //var listings = forEach property create a listing card?
+        return (
+            <div className='dashContainer'>
+                <div className='dashHead'>
+                    <div className="addButton">Add new property</div>
+                    <div className='desiredRentContainer'>
+                        <div>List properties with "desired rent" greator than: $</div>
+                        <input onChange={e=>updateDesiredRent(e.target.value)} type="number" name="desired rent" id=""/>
+                        <div onClick={}className='filterButton'>Filter</div>
+                        <div onClick={resetFilter()} className='resetButton'>Reset</div>
+                    </div>
+                </div>
+                <hr/>
+                <div className='homeListings'>
+                    <h2>Home Listings</h2>
+                    {/*listings*/}
+
+                </div>
+                
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+  const {
+
+  } = state;
+  return {
+     updateDesiredRent, resetFilter, 
+  };
+};
+
+export default connect(mapStateToProps, {
+    updateDesiredRent, resetFilter
+}
+)(Dashboard);
